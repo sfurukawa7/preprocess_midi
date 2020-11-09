391b856cbc5c6ff73d0f836f03e0974887c773c0
sfurukawa7 <shota.trp.musik711@gmail.com>
2020-11-09 20:56:21 +0900

    v0.01

--- src/preprocess_midi.py
+++ src/preprocess_midi.py
@@ -68,7 +68,6 @@ def piano_roll_to_pretty_midi(piano_roll, highest_note, lowest_note, fs, tempo, 
 #where to cut off high notes. 84 = C6
 #where to cut off low notes. 24=C1
 def load_rolls(file, smallest_note = 48, maximal_number_of_voices_per_track = 1, extracted_bar_num = 2, highest_note=84, lowest_note=24, notes_count_threshold = 0.5, max_velocity = 127, save_reconstructed_midi=False):
-    print(file)
     # MIDIをロード
     # 失敗した場合はNoneを返す
     try:
@@ -171,195 +170,105 @@ def load_rolls(file, smallest_note = 48, maximal_number_of_voices_per_track = 1,
         if instrument.program == 0 and instrument.is_drum == False: # ピアノのみ実行
             piano_instrument_list.append(instrument)
 
+    X_list = []
     if len(piano_instrument_list) > 0:
-        number_of_notes = []
-        piano_rolls = [i.get_piano_roll(fs=100) for i in piano_instrument_list]
-        for piano_roll in piano_rolls:
-            number_of_notes.append(np.count_nonzero(piano_roll))
-        
-        np_number_of_notes = np.array(number_of_notes)
-        chosen_piano_index = np_number_of_notes.argmax()
-        piano_instrument = piano_instrument_list[chosen_piano_index]
+        for piano_instrument in piano_instrument_list:
+            # number_of_notes = []
+            # piano_rolls = [i.get_piano_roll(fs=100) for i in piano_instrument_list]
+            # for piano_roll in piano_rolls:
+            #     number_of_notes.append(np.count_nonzero(piano_roll))
+            
+            # np_number_of_notes = np.array(number_of_notes)
+            # chosen_piano_index = np_number_of_notes.argmax()
+            # piano_instrument = piano_instrument_list[chosen_piano_index]
 
-        new_notes = [] # ピアノ音を入れるリスト
-        for note in piano_instrument.notes:
-            # 選択箇所区間に入っているかチェック
-            if note.start >= song_start and note.end <= song_end:
-                # 時刻を選択箇所内に揃える
-                note.start -= song_start
-                note.end -= song_start
-                new_notes.append(note)
+            new_notes = [] # ピアノ音を入れるリスト
+            for note in piano_instrument.notes:
+                # 選択箇所区間に入っているかチェック
+                if note.start >= song_start and note.end <= song_end:
+                    # 時刻を選択箇所内に揃える
+                    note.start -= song_start
+                    note.end -= song_start
+                    new_notes.append(note)
 
-        piano_instrument.notes = new_notes
-        print(piano_instrument)
+            piano_instrument.notes = new_notes
+
+            quarter_note_length = 60. / tempo
+            # fs：サンプリング周波数
+            # quarter_note_length * 4.：1小節の長さ(s)
+            # smallest_noteは最小音符(e.g. 16分音符ならsmallest_note = 16)
+            fs = 1./(quarter_note_length * 4./ smallest_note)
+            total_ticks = math.ceil(song_end*fs)
+
+            instrument = mid.instruments[0]
+            # np.ndarrayでpiano_rollを作成
+            piano_roll = np.zeros((total_ticks, 128))
+            for note in instrument.notes:
+                note_tick_start = note.start * fs
+                note_tick_end = note.end * fs
+                absolute_start = int(round(note_tick_start))
+                absolute_end = int(round(note_tick_end))
+                decimal = note_tick_start -absolute_start
+
+                # tick付近の音or1s以上の音ならばpiano_rollに追加(このため実際の音楽と少し異なる)
+                if decimal < 10e-3 or absolute_end-absolute_start >= 1:
+                    piano_roll[absolute_start:absolute_end, note.pitch] = note.velocity
+
+            cropped_piano_roll = piano_roll[:, lowest_note:highest_note]
+
+            extracted_bars_ticks = extracted_bar_num * smallest_note
+            total_ticks = cropped_piano_roll.shape[0]
+            if total_ticks % extracted_bars_ticks == 0:
+                total_extracted_bars_num = total_ticks / extracted_bars_ticks        
+                splited_piano_rolls = np.split(cropped_piano_roll, total_extracted_bars_num)
+            else:
+                padding_length = extracted_bars_ticks - (total_ticks % extracted_bars_ticks)
+                padded_piano_roll = np.pad(cropped_piano_roll, ((0, padding_length), (0,0)), 'constant', constant_values = (0,0)) # padding_length分だけパディング
+                padded_total_ticks = padded_piano_roll.shape[0]
+                total_extracted_bars_num = padded_total_ticks / extracted_bars_ticks        
+                splited_piano_rolls = np.split(padded_piano_roll, total_extracted_bars_num)
+
+            # 休符の多い小節を除去
+            new_piano_rolls = []
+            for splited_piano_roll in splited_piano_rolls:
+                notes_or_rests_list = [1 if one_tick_piano_roll.sum() > 0 else 0 for one_tick_piano_roll in splited_piano_roll]
+                notes_percent = sum(notes_or_rests_list) / len(splited_piano_roll)
+                print(notes_percent)
+
+                # 音数の閾値を超えたピアノロールのみappend
+                if notes_percent > notes_count_threshold:
+                    new_piano_rolls.append(splited_piano_roll)
+            print(new_piano_rolls)
+            if save_reconstructed_midi:
+                pathlib_file = Path(file)
+                file_stem = pathlib_file.stem
+                style_name = pathlib_file.parent.name
+
+                # 保存用ディレクトリを作成
+                reconstructed_data_dir = Path('./reconstructed_data') / style_name
+                if not reconstructed_data_dir.is_dir():
+                    reconstructed_data_dir.mkdir(parents=True)
+                
+                if piano_instrument.name in '.':
+                    track_name = piano_instrument.name.replace('.', '-')
+                else:
+                    track_name = piano_instrument.name
+
+                for i, new_piano_roll in enumerate(new_piano_rolls):
+                    reconstructed_mid = piano_roll_to_pretty_midi(new_piano_roll.T, highest_note, lowest_note, fs, tempo, has_cropped=True)
+                    reconstructed_file = reconstructed_data_dir / (file_stem + '_{}_{}.mid'.format(track_name, i))
+                    reconstructed_mid.write(str(reconstructed_file))
+
+            np_new_piano_rolls = np.array(new_piano_rolls)
+
+            X = np.where(np_new_piano_rolls > 0, 1, 0) # 学習用ピアノロール
+            X_list.append(X)
+
+        return X_list
+
     else:
         return None
-
-    quarter_note_length = 60. / tempo
-    # fs：サンプリング周波数
-    # quarter_note_length * 4.：1小節の長さ(s)
-    # smallest_noteは最小音符(e.g. 16分音符ならsmallest_note = 16)
-    fs = 1./(quarter_note_length * 4./ smallest_note)
-    total_ticks = math.ceil(song_end*fs)
-
-    instrument = mid.instruments[0]
-    # np.ndarrayでpiano_rollを作成
-    piano_roll = np.zeros((total_ticks, 128))
-    for note in instrument.notes:
-        note_tick_start = note.start * fs
-        note_tick_end = note.end * fs
-        absolute_start = int(round(note_tick_start))
-        absolute_end = int(round(note_tick_end))
-        decimal = note_tick_start -absolute_start
-
-        # tick付近の音or1s以上の音ならばpiano_rollに追加(このため実際の音楽と少し異なる)
-        if decimal < 10e-3 or absolute_end-absolute_start >= 1:
-            piano_roll[absolute_start:absolute_end, note.pitch] = note.velocity
-
-    cropped_piano_roll = piano_roll[:, lowest_note:highest_note]
-
-    extracted_bars_ticks = extracted_bar_num * smallest_note
-    total_ticks = cropped_piano_roll.shape[0]
-    if total_ticks % extracted_bars_ticks == 0:
-        total_extracted_bars_num = total_ticks / extracted_bars_ticks        
-        splited_piano_rolls = np.split(cropped_piano_roll, total_extracted_bars_num)
-    else:
-        padding_length = extracted_bars_ticks - (total_ticks % extracted_bars_ticks)
-        padded_piano_roll = np.pad(cropped_piano_roll, ((0, padding_length), (0,0)), 'constant', constant_values = (0,0)) # padding_length分だけパディング
-        padded_total_ticks = padded_piano_roll.shape[0]
-        total_extracted_bars_num = padded_total_ticks / extracted_bars_ticks        
-        splited_piano_rolls = np.split(padded_piano_roll, total_extracted_bars_num)
-
-    # 休符の多い小節を除去
-    new_piano_rolls = []
-    for splited_piano_roll in splited_piano_rolls:
-        notes_or_rests_list = [1 if one_tick_piano_roll.sum() > 0 else 0 for one_tick_piano_roll in splited_piano_roll]
-        notes_percent = sum(notes_or_rests_list) / len(splited_piano_roll)
-
-        # 音数の閾値を超えたピアノロールのみappend
-        if notes_percent > notes_count_threshold:
-            new_piano_rolls.append(splited_piano_roll)
-
-    if save_reconstructed_midi:
-        pathlib_file = Path(file)
-        file_stem = pathlib_file.stem
-        style_name = pathlib_file.parent.name
-
-        # 保存用ディレクトリを作成
-        reconstructed_data_dir = Path('./reconstructed_data') / style_name
-        if not reconstructed_data_dir.is_dir():
-            reconstructed_data_dir.mkdir(parents=True)
-        
-        for i, new_piano_roll in enumerate(new_piano_rolls):
-            reconstructed_mid = piano_roll_to_pretty_midi(new_piano_roll.T, highest_note, lowest_note, fs, tempo, has_cropped=True)
-            reconstructed_file = reconstructed_data_dir / (file_stem + '_{}.mid'.format(i))
-            reconstructed_mid.write(str(reconstructed_file))
-
-    np_new_piano_rolls = np.array(new_piano_rolls)
-    X = np.where(np_new_piano_rolls > 0, 1, 0) # 学習用ピアノロール
-    return X
-    # # 各楽器のprogram numberを入手 
-    # # program number:0~127の楽器と一対一で対応する番号
-    # programs = [i.program for i in mid.instruments]
-
-    # if print_anything:print(max_concurrent_notes_per_track_list)
-    # # 事前に設定した最大発音数を超えないようにするためのリスト
-    # override_max_notes_per_track_list = [maximal_number_of_voices_per_track for _ in max_concurrent_notes_per_track_list] 
-    # # トラック数をmax_voice個に制限したときに生じる無音のトラック数(そもそも元々のトラック数がmax_voice未満の場合、無音のトラックが生じる)
-    # silent_tracks_if_we_dont_override = config.MAX_VOICES - sum([min(maximal_number_of_voices_per_track, x) if x > 0 else 0 for x in max_concurrent_notes_per_track_list[:config.MAX_VOICES]])
-
-    # if print_anything:print("Silent tracks if no override:", silent_tracks_if_we_dont_override)
-    # # 「silent_tracks_if_we_dont_overrideが存在andあるトラックの最大発音数が設定よりオーバーしている」とき
-    # # ->silent_tracks_if_we_dont_override分だけオーバーしているトラックの最大発音数の上限を上げることができる
-    # for voice in range(min(config.MAX_VOICES, len(max_concurrent_notes_per_track_list))):
-    #     if silent_tracks_if_we_dont_override > 0 and max_concurrent_notes_per_track_list[voice] > maximal_number_of_voices_per_track:
-    #         additional_voices = min(silent_tracks_if_we_dont_override, max_concurrent_notes_per_track_list[voice]-maximal_number_of_voices_per_track)
-    #         override_max_notes_per_track_list[voice] += additional_voices
-    #         silent_tracks_if_we_dont_override -= additional_voices
-    # if print_anything:print("Override programs:", override_max_notes_per_track_list)
-
-    # # 発音数を最大同時発音数以内にするためにpiano_rollを選択する
-    # chosen_piano_rolls = []
-    # chosen_velocity_rolls = []
-    # chosen_held_note_rolls = []
-    # chosen_programs = []
-    # max_song_length = 0
-
-    # for piano_roll, velocity_roll, held_note_roll, program, max_concurrent_notes, override_max_notes_per_track in zip(piano_rolls, velocity_rolls, held_note_rolls, programs, max_concurrent_notes_per_track_list, override_max_notes_per_track_list):
-    #     if max_concurrent_notes > 0:
-    #         if include_only_monophonic_instruments:
-    #             if max_concurrent_notes > 1:
-    #                 if print_anything:print("Skipping this piano roll since it's polyphonic. Prigram number ", program)
-    #                 continue
-    #             else:
-    #                 pass
-    #                 if print_anything:print("Adding monophonic program number: ", program)
-                
-    #             monophonic_piano_roll = piano_roll
-
-    #             if len(chosen_piano_rolls) < config.MAX_VOICES:
-    #                 chosen_piano_rolls.append(monophonic_piano_roll)
-    #                 chosen_velocity_rolls.append(velocity_roll)
-    #                 chosen_held_note_rolls.append()
-    #                 chosen_programs.append(program)
-    #                 if monophonic_piano_roll.shape[0] > max_song_length:
-    #                     max_song_length = monophonic_piano_roll.shape[0]
-    #                 else:
-    #                     break
-    #         else: # 初期設定ではこちら
-    #             # トラックあたりのボイス数を、トラックあたりの実際の同時使用ボイスの最小値、または設定ファイルで許可されている最大値で制限
-    #             for voice in range(min(max_concurrent_notes, max(maximal_number_of_voices_per_track, override_max_notes_per_track))):
-    #                 # 最高音をvoice 0, その次をvoice 1, ...とする
-    #                 monophonic_piano_roll = np.zeros(piano_roll.shape)
-    #                 for step in range(piano_roll.shape[0]):
-    #                     # 音の高い順に並び替え
-    #                     notes = np.nonzero(piano_roll[step, :])[0][::-1]
-    #                     if len(notes) > voice:
-    #                         monophonic_piano_roll[step, notes[voice]] = 1
-                    
-    #                 # これらをchosenに追加する
-    #                 if len(chosen_piano_rolls) < config.MAX_VOICES:
-    #                     chosen_piano_rolls.append(monophonic_piano_roll)
-    #                     chosen_velocity_rolls.append(velocity_roll[:, voice])
-    #                     chosen_held_note_rolls.append(held_note_roll[:, voice])
-    #                     chosen_programs.append(program)
-    #                     if monophonic_piano_roll.shape[0] > max_song_length:
-    #                         max_song_length = monophonic_piano_roll.shape[0]
-    #                 else:
-    #                     break
-    #             if len(chosen_piano_rolls) == config.MAX_VOICES:
-    #                 break
     
-    # assert(len(chosen_piano_rolls) == len(chosen_velocity_rolls))
-    # assert(len(chosen_piano_rolls) == len(chosen_held_note_rolls))
-    # assert(len(chosen_piano_rolls) == len(chosen_programs))
-
-    # if len(chosen_piano_rolls) > 0:
-    #     song_length = max_song_length*config.MAX_VOICES
-
-    #     # Yの準備
-    #     # Y:ターゲットノート
-    #     Y = np.zeros((song_length, chosen_piano_rolls[0].shape[1]))
-    #     # テンソルとなっているピアノロールを展開して一つのマトリックスに
-    #     for i, piano_roll in enumerate(chosen_piano_rolls):
-    #         for step in range(piano_roll.shape[0]):
-    #             Y[i + step*config.MAX_VOICES, :] += piano_roll[step, :]
-        
-    #     for step in range(Y.shape[0]):
-    #         # 演奏される音は最大でも1音
-    #         assert(np.sum(Y[step, :]) <= 1)
-    #     # 想定する音高の範囲で切り取る
-    #     # これにより特徴空間を大幅に削減できる
-    #     Y = Y[:, config.LOW_CROP:config.HIGH_CROP]
-    #     # 無音を追加
-    #     if include_silent_note:
-    #         Y = np.append(Y, np.zeros((Y.shape[0], 1)), axis=1)
-    #         for step in range(Y.shape[0]):
-    #             if np.sum(Y[step]) == 0:
-    #                 Y[step, -1] = 1
-    #         # 各ステップのどこかには1があるようにする
-    #         for step in range(Y.shape[0]):
-    #             assert(np.sum(Y[step, :] == 1))
 
 def import_midi_from_folder(folder, load_from_npy_instead_of_midi, save_imported_midi_as_npy, save_reconstructed_midi, test_fraction):
     # npyでロードする場合
@@ -403,10 +312,11 @@ def import_midi_from_folder(folder, load_from_npy_instead_of_midi, save_imported
                 C = i
                         
                 if X is not None:
-                    X_list.append(X)
-                    c_classes.append(C)
-                    paths.append(str(file))
-                    no_imported += 1
+                    for x in X:
+                        X_list.append(x)
+                        c_classes.append(C)
+                        paths.append(str(file))
+                        no_imported += 1
 
         # if no_imported >= config.MAX_SONGS:
         #     break
